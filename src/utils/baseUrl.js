4d0d2079b5754ec3b748e46694171f24e376adb2
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-06-19 20:06:21 +0600

    chore: fix conflict

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,8 +1,3 @@
 export const getBaseUrl = () => {
-<<<<<<< HEAD
   return "145.223.75.211:3003"; // replace with your API base URL
-=======
-  return "145.223.75.211:6007";
-  // return "http://10.0.60.126:6007";
->>>>>>> 2e44369c599bf1c92c7fd66327bebf00ca3aed7d
 };
--- vite.config.js
+++ vite.config.js
@@ -15,16 +15,11 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
-<<<<<<< HEAD
     host: "145.223.75.211",
     port: 3003,
-=======
-    host: "31.97.128.5",
-    port: 3009,
   },
   build: {
     sourcemap: false, // disables source maps for production
     minify: "esbuild", // faster minifier
->>>>>>> 2e44369c599bf1c92c7fd66327bebf00ca3aed7d
   },
 });
