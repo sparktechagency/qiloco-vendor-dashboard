2e44369c599bf1c92c7fd66327bebf00ca3aed7d
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-06-19 18:13:25 +0600

    vite config qiloco vendor dashboard

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: "10.0.60.210",
+//     port: 3009,
+//   },
+// });
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "10.0.60.210",
+    host: "31.97.128.5",
     port: 3009,
+  },
+  build: {
+    sourcemap: false, // disables source maps for production
+    minify: "esbuild", // faster minifier
   },
 });
